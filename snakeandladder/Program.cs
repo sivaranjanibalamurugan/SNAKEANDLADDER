3d43478c705ded18c5045cf1732cf6b288d18d47
unknown <sivaranjanib99@gmail.com>
2021-06-29 22:58:40 +0530

    [siva] check for options

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -4,16 +4,33 @@ namespace snakeandladder
 {
     class Program
     {
-       
+        public const int ladder = 2;
+        public const int snake = 1;
+        public const int no_play = 0;
         static void Main(string[] args)
-        { 
-               Console.WriteLine("Welcome to Snake and Ladder World");
+        {
+            Console.WriteLine("Welcome to Snake and Ladder World");
             int player = 1;
             int position = 0;
             Random random = new Random();
             int dieInput = random.Next(1, 7);
-            Console.WriteLine("Player " + player + " is in position " + position  + " initially");
+            int choice = random.Next(1, 3);
+            switch (choice)
+            {
+                case ladder:
+                    Console.WriteLine("move forward ");
+                    break;
+                case snake:
+                    Console.WriteLine("move backward");
+                    break;
+                case no_play:
+                    Console.WriteLine("don't move stay");
+                    break;
+            }
+
+            Console.WriteLine("Player " + player + " is in position " + position + " initially");
             Console.Read();
-        }
+        }    
+        
     }
 }
