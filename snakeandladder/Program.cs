4ff313b1339faa1e486b30cdda4cc1ef5960462d
unknown <sivaranjanib99@gmail.com>
2021-06-29 21:27:48 +0530

    [siva] welcome

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -4,13 +4,11 @@ namespace snakeandladder
 {
     class Program
     {
-        public const int player = 1;
-
+       
         static void Main(string[] args)
-        {
-            int position = 0;
-            Random random = new Random();
-            int deiInput = random.Next(1, 6);
+        { 
+               Console.WriteLine("Welcome to Snake and Ladder World");
+            Console.Read();
         }
     }
 }
