8162143172c79e71d7cb34cb437cb30ba44927fd
unknown <sivaranjanib99@gmail.com>
2021-06-30 22:13:55 +0530

    [siva] to find who won the play

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -4,78 +4,72 @@ namespace snakeandladder
 {
     class Program
     {
-        //public const int ladder = 2;
-        // public const int snake = 1;
-        //  public const int no_play = 0;
+        public const int ladder = 2;
+         public const int snake = 1;
+        public const int no_play = 0;
          public const int die_RollTime = 0;
-        // public const int Win_position = 100;
-        int ChoiceOfPlayer = 0;
+         public const int Win_position = 100;
+       // public const int position = 0;
+        //int ChoiceOfPlayer = 0;
         
-        public void StepToWin()
-        {
+          static void Main(string[] args)
+          {
+            int position = 0 ; //local variable 
+            int player = 1 ;
+            //int Choice = 3;
+           // int Ladder = 2;
+            int count = 0;
+            int i = 0;
+          //  int pos;
+            //int WIN = 1;
+           //int PlayerChoice;
             Random random = new Random();
-            int[] playerPos = new int[2];
-            int[] dieRollTime = new int[2];
-            int player;
-            int pos;
-            int WIN;
-            int PlayerChoice;
+            int dieInput = random.Next(1, 7);
+            int choice = random.Next(1, 3);
+            while (i>0 || i<=100 )
+                switch (choice)
+            { 
+                case ladder:
+                    Console.WriteLine("move forward ");
+                    break;
+                case snake:
+                    Console.WriteLine("move backward");
+                    break;
+                case no_play:
+                    Console.WriteLine("don't move stay");
+                    break;
+            }
+
+            {      if (position < 100)
+                    {
+                        Console.WriteLine("move by adding the number ");
+                    }
+                    else if (position == 100)
+                    {
+                    Console.WriteLine("stay there");
+                     
+                    }
+                    else
+                    {
+                        position = position++;
+                    }
+
+
+                    count++;
+            }
             {
-           
-                    
-       while (playerPos[0] < ChoiceOfPlayer.WIN && playerPos[1]< ChoiceOfPlayer.WIN)
-            {
-                player = 0;
-                while (player <= 1)
+                if (int position = Win_position)
+               {
+                    Console.WriteLine("Player 1 win at " + die_RollTime + " move");
+               }
+            
+                else
                 {
-                    pos = playerPos[player];
-
-                    //Generating DieRoll using Next method
-                    int dieRoll = random.Next(1, 7);
-
-                    //Generating the player choice
-                    int playerChoice = random.Next(0, 3);
-
-                    //current position of player
-                    pos = ChoiceOfPlayer.PlayerChoice(playerChoice, pos, dieRoll);
-                    playerPos[player]=pos;
-
-                    Console.WriteLine("Position of player "+(player+1)+" is : "+playerPos[player]);
-
-                    dieRollTime[player]++;
-
-                    //checks whether player reache win
-                    if (playerPos[player] == 100)
-                    {
-                        break;
-                    }
-
-                    //If choice is ladder then player can take two turns
-                    if (playerChoice!=ChoiceOfPlayer.LADDER )
-                    {
-                        player++;
-                    }
-
-
+                    Console.WriteLine("Player 2 win at " + die_RollTime + " move");
                 }
             }
-            if(playerPos[0]==ChoiceOfPlayer.WIN)
-            {
-                Console.WriteLine("Player 1 win at "+dieRollTime[0]+" move");
-            }
-            else
-            {
-                Console.WriteLine("Player 2 win at "+dieRollTime[1]+" move");
-            }
-
-        }
-
-    }
-
-}
-            Console.Read();
-
-
-        }
+           
+               Console.Read();
+          }
     }
 }
